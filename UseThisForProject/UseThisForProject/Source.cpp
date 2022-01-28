018c73ad03002fb4f220925babc8b7e1d236e08f
Marnie Hackbarth <573918@students.klamathcc.edu>
2022-01-27 16:55:23 -0800

    actual initial check in

---
+++ UseThisForProject/UseThisForProject/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+using std::ifstream;
+using std::ofstream;
+
+
+int main()
+{
+int Level1[1] = { 0 };
+int Level2[1] = { 0 };
+int Level3[1] = { 0 };
+int GameBoard1[11][11] = { 0 };
+int GameBoard2[11][11] = { 0 };
+
+int Carrier1[] = { 5 };
+int Battleship1[] = { 4 };
+int Cruiser1[] = { 3 };
+int Submarine1[] = { 3 };
+int Destroyer1[] = { 2 };
+int PatrolBoat1[] = { 2 };
+
+
+
+
+
+
+
+	return 0;
+}
